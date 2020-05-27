e410eea6649a82894ed8681a1926abdd4280329c
Matthew Baldwin <mgbaldwin@gmail.com>
2020-05-26 19:49:53 -0700

    Take a filename as a parameter to run, or oscilate

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,36 @@ use gol::Universe;
 fn main() {
     let half_second = time::Duration::from_millis(500);
 
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => run_file(&args[1], &half_second),
+        _ => oscillator(&half_second),
+    }
+}
+
+fn oscillator(period: &time::Duration) {
     let mut board = Universe::new(5, 5);
 
     board.bless_cell(3, 1);
     board.bless_cell(3, 2);
     board.bless_cell(3, 3);
 
-    for _ in 0..128 {
+    run_board(&mut board, &period, 10);
+}
+
+fn run_board(board: &mut Universe, period: &time::Duration, loops: u32) {
+    for _ in 0..loops {
         print!("\x1B[2J");
         println!("{}", board);
 
-        thread::sleep(half_second);
+        thread::sleep(*period);
         board.tick();
     }
+}
+
+fn run_file(filename: &String, period: &time::Duration) {
+    let mut board = Universe::from_file(filename).unwrap();
+
+    run_board(&mut board, &period, 30);
 }
