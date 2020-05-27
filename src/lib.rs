01e23b1b116e5dbf46c97e8d0749e2a8a2f9ce55
Matthew Baldwin <mgbaldwin@gmail.com>
2020-05-26 19:58:44 -0700

    Use unicode black square centered character to mark a live cell

--- src/lib.rs
+++ src/lib.rs
@@ -146,7 +146,7 @@ impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for line in self.cells.as_slice().chunks(self.width as usize) {
             for &cell in line {
-                let symbol = if cell == Cell::Dead { ' ' } else { 'X' };
+                let symbol = if cell == Cell::Dead { ' ' } else { '\u{2BC0}' };
                 write!(f, "{}", symbol)?;
             }
             write!(f, "\n")?;
