6b703e433c739094eee162005adf7866cde3efd9
Matthew Baldwin <mgbaldwin@gmail.com>
2020-05-27 20:22:42 -0700

    Simplify by using structopt for command args

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,59 @@
-extern crate clap;
-#[macro_use] extern crate tempus_fugit;
+#[macro_use]
+extern crate tempus_fugit;
 
-use clap::{Arg, App};
+use structopt::StructOpt;
 
-use std::path::Path;
+use std::path::PathBuf;
 use std::{thread, time};
 
 use gol::Universe;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "gol", about)]
+struct Opt {
+    /// Be verbose
+    #[structopt(short, long)]
+    verbose: bool,
+
+    /// Time (in ms) to sleep between renderings
+    #[structopt(short, long, default_value = "500")]
+    interval: u64,
+
+    /// Number of iterations to run
+    #[structopt(short = "t", long, default_value = "30")]
+    iterations: u32,
+
+    /// Runs a benchmark (no rendering, no sleeping)
+    #[structopt(short, long)]
+    benchmark: bool,
+
+    /// File to seed the universe with
+    file_name: Option<PathBuf>,
+}
+
 fn main() {
+    let opt = Opt::from_args();
 
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-            .version(env!("CARGO_PKG_VERSION"))
-            .author(env!("CARGO_PKG_AUTHORS"))
-            .about(env!("CARGO_PKG_DESCRIPTION"))
-            .arg(Arg::with_name("file")
-                .value_name("FILE")
-                .help("Use FILE as seed for universe")
-                .takes_value(true)
-                .index(1))
-            .arg(Arg::with_name("interval")
-                .short("i")
-                .long("interval")
-                .value_name("TIME_MS")
-                .help("Time (in ms) to sleep between renderings")
-                .takes_value(true))
-            .arg(Arg::with_name("benchmark")
-                .short("b")
-                .long("benchmark")
-                .help("Runs a benchmark (no rendering, no sleeping"))
-            .arg(Arg::with_name("iterations")
-                .short("t")
-                .long("iterations")
-                .value_name("ITERATIONS")
-                .help("Number of iterations to run for")
-                .takes_value(true))
-            .arg(Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .help("Be verbose"))
-            .get_matches();
+    println!("{:?}", opt);
 
-    let interval = matches.value_of("interval").unwrap_or("500");
-    let interval = time::Duration::from_millis(interval.parse::<u64>().unwrap());
-    let iterations = matches.value_of("iterations").unwrap_or("30");
-    let iterations = iterations.parse::<u32>().unwrap();
-
-    let verbose = matches.is_present("verbose");
-    let bench = matches.is_present("benchmark");
+    let interval = time::Duration::from_millis(opt.interval);
+    let iterations = opt.iterations;
 
     let mut universe = oscillator();
 
-    if let Some(filename) = matches.value_of("file") {
-        if verbose { println!("Seeding universe from {}", filename); }
+    if let Some(filename) = opt.file_name {
+        if opt.verbose {
+            println!("Seeding universe from {}", filename.to_str().unwrap());
+        }
 
-        let file = Path::new(&filename);
+        let file = filename.as_path();
         universe = Universe::from_file(&file).unwrap();
     }
 
-    if bench {
+    if opt.benchmark {
         println!("Running benchmark with {} iterations", iterations);
 
-        if verbose {
+        if opt.verbose {
             println!("Initial state:");
             println!("{}", universe);
         }
@@ -71,14 +63,14 @@ fn main() {
             universe
         }};
 
-        if verbose {
+        if opt.verbose {
             println!("End state:");
             println!("{}", universe);
         }
 
         println!("Run {} iterations in {}", iterations, measurement);
     } else {
-        run_board(&mut universe, &interval, iterations, verbose);
+        run_board(&mut universe, &interval, iterations, opt.verbose);
     }
 }
 
@@ -96,7 +88,9 @@ fn run_board(board: &mut Universe, interval: &time::Duration, iterations: u32, v
     for i in 0..iterations {
         print!("\x1B[2J");
 
-        if verbose { println!("Generation {} of {}", (i + 1), iterations); }
+        if verbose {
+            println!("Generation {} of {}", (i + 1), iterations);
+        }
 
         println!("{}", board);
 
